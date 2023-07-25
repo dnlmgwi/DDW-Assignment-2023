08d6b8e3d2992a44ebbdd8a356189562f3a03639
Daniel P. Mgawi <54575392+dnlmgwi@users.noreply.github.com>
2023-07-25 12:32:51 +0200

    refactoring: Added New Element and Fixes
    - Icons Added
    - Images Added
    - Using Json and JS to populate page content
    - Pages Added to Nav

---
+++ js/fetchSpecialOffers.js
@@ -0,0 +1,31 @@
+function appendData(data) {
+  let cardContainer = document.getElementById("product-list");
+  data.forEach(element => {
+    let div = document.createElement("div");
+    div.innerHTML = `
+    <div id="${element.id}" class="card">
+      <div class="product-box">
+        <img src="${element.imageUrl}">
+      </div>
+      <div class="card-text">
+        <p class="product-name">${element.productName}</p>
+        <div class="price-text">
+          <p class="currency">MK</p>
+          <p class="price">${element.price}</p><div>
+        </div>                      
+    </div>
+    <button class="add-to-basket-button">Add To Basket</button>  
+    `;
+    cardContainer.appendChild(div);
+  });
+}
+
+fetch('/data/specialOffers.json')
+  .then(res => res.json())
+  .then((res) => {
+    const data = res.data;
+    console.log("Data:" + data);
+    appendData(data);
+  }).catch(function (err) {
+    console.log("Error:" + err)
+  });;
